47a97d0b79233c0777b2fc3650194fe45e0beb51
YURII <trohumchyk@gmail.com>
2015-06-15 08:59:25 +0300

    add View and Controller

---
+++ IDAPCourseUI/Source/TYVSquareView.m
@@ -0,0 +1,21 @@
+//
+//  TYVSquareView.m
+//  IDAPCourseUI
+//
+//  Created by YURII on 15.06.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import "TYVSquareView.h"
+
+@implementation TYVSquareView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
