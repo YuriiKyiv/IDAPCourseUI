746676d952919aeab2e48b0125f0b0163956c3f3
YURII <trohumchyk@gmail.com>
2015-06-18 16:40:41 +0300

    Image model

---
+++ IDAPCourseUI/Source/Models/TYVImageModel/TYVImageModel.h
@@ -0,0 +1,13 @@
+//
+//  TYVImageModel.h
+//  IDAPCourseUI
+//
+//  Created by YURII on 18.06.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYVImageModel : NSObject
+
+@end
