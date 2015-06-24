63af2fab678945ffcbb274919b714ff26c6ed2a7
YURII <trohumchyk@gmail.com>
2015-06-24 12:16:41 +0300

    add data info

---
+++ IDAPCourseUI/Source/Library/Classes/TYVDataArrayModelInfo/TYVDataArrayModelInfo.h
@@ -0,0 +1,13 @@
+//
+//  TYVDataArrayModelInfo.h
+//  IDAPCourseUI
+//
+//  Created by YURII on 24.06.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TYVDataArrayModelInfo : NSObject
+
+@end
---
+++ IDAPCourseUI/Source/Library/Classes/TYVDataArrayModelInfo/TYVDataArrayModelInfo.m
@@ -0,0 +1,13 @@
+//
+//  TYVDataArrayModelInfo.m
+//  IDAPCourseUI
+//
+//  Created by YURII on 24.06.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import "TYVDataArrayModelInfo.h"
+
+@implementation TYVDataArrayModelInfo
+
+@end
