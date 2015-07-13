f6f44247be52ebda394a4783607d37da444ce590
YURII <trohumchyk@gmail.com>
2015-07-13 11:58:14 +0300

    TYVImageCache

--- IDAPCourseUI/Source/Library/Classes/TYVImageCache/TYVImageCache.h
+++ IDAPCourseUI/Source/Library/Classes/TYVImageCache/TYVImageCache.h
@@ -9,5 +9,11 @@
 #import <Foundation/Foundation.h>
 
 @interface TYVImageCache : NSObject
+@property (nonatomic, readonly) NSMapTable  *cacheMapTable;
+
++ (instancetype)sharedImageChace;
+
+- (void)addObject:(id)object forKey:(id)key;
+- (id)objectForKey:(id)key;
 
 @end
--- IDAPCourseUI/Source/Library/Classes/TYVImageCache/TYVImageCache.m
+++ IDAPCourseUI/Source/Library/Classes/TYVImageCache/TYVImageCache.m
@@ -8,6 +8,63 @@
 
 #import "TYVImageCache.h"
 
+@interface TYVImageCache ()
+@property (nonatomic, strong)   NSMapTable  *cache;
+
+@end
+
 @implementation TYVImageCache
+
+@dynamic cacheMapTable;
+
+#pragma mark -
+#pragma mark Class Methods
+
++ (instancetype)sharedImageChace {
+    static TYVImageCache *__sharedObject = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        __sharedObject = [[self alloc] init];
+    });
+    
+    return __sharedObject;
+}
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        self.cache = [NSMapTable weakToWeakObjectsMapTable];
+    }
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accesors
+
+- (NSMapTable *)cacheMapTable {
+    @synchronized (self) {
+        return [self.cache copy];
+    }
+}
+
+
+#pragma mark -
+#pragma mark Public Methods
+
+- (void)addObject:(id)object forKey:(id)key {
+    @synchronized (self) {
+        [self.cache setObject:object forKey:key];
+    }
+}
+
+- (id)objectForKey:(id)key {
+    @synchronized (self) {
+        return [self.cache objectForKey:key];
+    }
+}
 
 @end
