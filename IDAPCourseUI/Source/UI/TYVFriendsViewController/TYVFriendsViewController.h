44bed6d7686bfceca63f88574e631e16829e222e
YURII <trohumchyk@gmail.com>
2015-07-22 10:57:06 +0300

    init controllers

---
+++ IDAPCourseUI/Source/UI/TYVFriendsViewController/TYVFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  TYVFriendsViewController.h
+//  IDAPCourseUI
+//
+//  Created by YURII on 22.07.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TYVFriendsViewController : UIViewController
+
+@end
---
+++ IDAPCourseUI/Source/UI/TYVLoginViewController/TYVLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  TYVLoginViewController.h
+//  IDAPCourseUI
+//
+//  Created by YURII on 22.07.15.
+//  Copyright (c) 2015 YURII. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TYVLoginViewController : UIViewController
+
+@end
